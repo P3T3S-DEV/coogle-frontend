4c82e5d4cde44398479c8d7ed631d9d92413db42
Yeison Garzón T <toxiicsa@hotmail.com>
2021-03-03 07:31:11 -0500

    ADD: home component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ChatService } from 'src/app/http/chat.service';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -7,21 +6,4 @@ import { ChatService } from 'src/app/http/chat.service';
   styleUrls: ['./home.component.scss']
 })
 
-export class HomeComponent implements OnInit{
-
-  public listMessages: string[] = [];
-  @ViewChild('inputMessage') private inputText!: ElementRef;
-  constructor(private chatService: ChatService){}
-
-  ngOnInit(){
-    this.chatService.listen('server:message').subscribe((data: any)=>{
-      console.log(data);
-      this.listMessages.push(data);
-    })
-  }
-
-  submitMessage(){
-    this.chatService.emit('client:message', this.inputText.nativeElement.value);
-    this.inputText.nativeElement.value = "";
-  }
-}
+export class HomeComponent{}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { ChatService } from 'src/app/http/chat.service';
-import { HttpClientModule } from '@angular/common/http';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  declarations: [HomeComponent, NavbarComponent],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    HttpClientModule
-  ],
-  providers:[
-    ChatService
+    CoreModule
   ]
 })
 export class HomeModule { }
