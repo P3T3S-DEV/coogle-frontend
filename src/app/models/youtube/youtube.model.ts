fbf5cd682c6326573d731c0e72bee1a7c3b512e5
Yeison Garzón T <toxiicsa@hotmail.com>
2021-03-12 01:36:50 -0500

    ADD: youtube model

---
+++ src/app/models/youtube/youtube.model.ts
@@ -0,0 +1,50 @@
+export interface Video {
+    kind: string;
+    etag: string;
+    nextPageToken: string;
+    regionCode: string;
+    pageInfo: PageInfo;
+    items: Item[];
+}
+
+export interface PageInfo {
+    totalResults: number;
+    resultsPerPage: number;
+}
+
+export interface Item {
+    kind: string;
+    etag: string;
+    id: ID;
+    snippet: Snippet;
+}
+
+export interface ID {
+    kind: string;
+    videoId: string;
+}
+
+export interface Snippet {
+    publishedAt: Date;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: Thumbnails;
+    channelTitle: string;
+    liveBroadcastContent: string;
+    publishTime: Date;
+}
+
+export interface Thumbnails {
+    default: Default;
+    medium: Default;
+    high: Default;
+}
+
+export interface Default {
+    url: string;
+    width: number;
+    height: number;
+}
+
+
