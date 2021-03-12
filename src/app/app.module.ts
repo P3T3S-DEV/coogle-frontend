30f42e0a5dff75dd929b81e48569d7e3902d72c0
Yeison Garzón T <toxiicsa@hotmail.com>
2021-03-12 01:36:16 -0500

    ADD: chat guard in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatGuard } from './guards/chat/chat.guard';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
+  ],
+  providers: [
+    ChatGuard
   ],
   bootstrap: [AppComponent]
 })
