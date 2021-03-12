2f96ca22c025cda601b4be7581e22984fb5ca288
Yeison Garzón T <toxiicsa@hotmail.com>
2021-03-12 01:37:06 -0500

    ADD: core module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { CoreModule } from '../core/core.module';
 import { SigninComponent } from './sign-in/signin.component';
 import { SignupComponent } from './sign-up/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    CoreModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CoreModule
   ]
 })
 export class AuthModule { }
