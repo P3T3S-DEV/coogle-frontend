2b8ecc6a62fcca5698b036ce860abb1a24d3f61b
Yeison Garzón T <toxiicsa@hotmail.com>
2021-03-12 02:25:54 -0500

    FIX: deploy configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_API_URL: "https://coogle-chat.herokuapp.com/api/",
+  YOUTUBE_BASE_URL: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=video&key=AIzaSyBYBvtuG40vx0frrmOdykzLJ-Udf-4kH6w&q=`,
 };
