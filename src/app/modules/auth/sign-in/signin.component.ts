e5502ccfe6eb79f9761ddc60c453c021c23b8ee1
Yeison Garzón T <toxiicsa@hotmail.com>
2021-03-13 00:04:48 -0500

    FIX: format document

--- src/app/modules/auth/sign-in/signin.component.ts
+++ src/app/modules/auth/sign-in/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { SigninService } from 'src/app/http/auth/signin/signin.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -8,53 +8,55 @@ import { Router } from '@angular/router';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent{
+export class SigninComponent {
 
-   public errorMessage: string = "";
-   public hasError: boolean = false;
+  public errorMessage: string = "";
+  public hasError: boolean = false;
 
-   signinForm = new FormGroup({
-    username: new FormControl('',[
-    Validators.required,
-    Validators.minLength(4),
-    Validators.maxLength(20)]),
-    password: new FormControl('',[
-    Validators.required,
-    Validators.minLength(4),
-    Validators.maxLength(20)
+  signinForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
     ]),
   });
+
   constructor(
     private _signinService: SigninService,
     private router: Router,
   ) { }
-  get getInputUsername(){
+  get getInputUsername() {
     return this.signinForm.get('username');
   }
   get getInputPassword() {
     return this.signinForm.get('password');
   }
 
-  sendSignin(){
+  sendSignin() {
     this._signinService.postSignin(
       this.getInputUsername?.value,
       this.getInputPassword?.value,
     ).subscribe(
-      data =>{
+      data => {
         this.hasError = false;
         localStorage.setItem('username', data.user.username);
         localStorage.setItem('token', data.token);
         this.router.navigateByUrl('/chat');
-    },
-    error =>{
-      this.errorMessage = error;
-      this.hasError = true;
-      this.router.navigateByUrl('/auth/signin');
-    });
+      },
+      error => {
+        this.errorMessage = error;
+        this.hasError = true;
+        this.router.navigateByUrl('/auth/signin');
+      });
     this.router.navigateByUrl('/chat');
   }
 
-  timing(){
-    setTimeout(()=>this.hasError = false, 5000);
+  timing() {
+    setTimeout(() => this.hasError = false, 5000);
   }
+
 }
